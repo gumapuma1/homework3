1f44d5254b7a9e6bccc690a2142d0475f395e4e3
gumapuma1 <gumapumaa@gmail.com>
2020-06-02 07:41:47 +0300

    Task 2

---
+++ week3_2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+const int n = 10;
+
+int main() {
+    int arr[n], sum = 0;
+    srand(6527);
+
+    for(int i = 0; i < n; i++) {
+        arr[i] = rand() % 20;
+    }
+
+    cout << "Array: ";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+        if(arr[i] > 5) sum+=arr[i];
+    }
+    cout << endl << "Sum (>5) = " << sum << endl;
+
+    return 0;
+}
